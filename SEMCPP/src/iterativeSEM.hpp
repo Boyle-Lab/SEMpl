c0fc09b06e93ac7ca2003f0bced1af9bf1cc263e
Cody Morterud <cody22597@gmail.com>
2016-11-09 14:20:58 -0500

    Initial commit

---
+++ SEMCPP/src/iterativeSEM.hpp
@@ -0,0 +1,14 @@
+//
+//  iterativeSEM.hpp
+//  SEMCPP
+//
+//  Created by Cody Morterud on 11/9/16.
+//  Copyright © 2016 Boyle Lab. All rights reserved.
+//
+
+#ifndef iterativeSEM_hpp
+#define iterativeSEM_hpp
+
+#include <stdio.h>
+
+#endif /* iterativeSEM_hpp */
