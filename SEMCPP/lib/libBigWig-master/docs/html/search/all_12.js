2004a192531b432affffea24189284c2496bc693
Cody Morterud <cody22597@gmail.com>
2016-11-18 11:27:50 -0500

    Added the suitable library

---
+++ SEMCPP/lib/libBigWig-master/docs/html/search/all_12.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['writebuffer',['writeBuffer',['../structbigWigFile__t.html#abd73667391749fbfd49c96105f643133',1,'bigWigFile_t']]]
+];
---
+++ SEMCPP/lib/libBigWig-master/docs/html/search/all_c.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['p',['p',['../structbwWriteBuffer__t.html#adbd1ed0f439084b2b6839ddc5609e793',1,'bwWriteBuffer_t']]]
+];
