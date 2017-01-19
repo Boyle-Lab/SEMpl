b2f84378017bac74c208bc2cb6dadbb3d65bfcc2
Cody Morterud <cody22597@gmail.com>
2017-01-19 14:01:55 -0500

    Added notes about bowtie. Ideally, Colten should begin working on step 3 of SNPEffectMatrix.cpp, as I am on step 2.

--- SEMCPP/src/bowtie_genome_map.cpp
+++ SEMCPP/src/bowtie_genome_map.cpp
@@ -7,16 +7,5 @@ void split(std::string str, std::string splitBy, std::vector<std::string>& token
 // at the end of the command                              // intermediate.dat, as
 //                                                        // defined in iterativeSEM.hpp
 void bowtie_genome_map(Dataset &data, int length, const string& genome){
-  // string command = "./bin/bowtie --quiet -a -v 0 ./data/" + genome + " -f " + output_file;
-  // ifstream IN(input_to_this_file);
-  // string line = "";
-  // vector<string> dat, output;
-  // while (getline(IN, line, '\n')) {
-  //   split(line, '\t', dat);
-  //
-  //   // UNFINISHED
-  //   // UNFINISHED
-  //   // UNFINISHED
-  // }
-
+  // investigating bowtie
 }
