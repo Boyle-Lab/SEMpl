e6f88dffdd8b40d660b64d429c84d100f576e995
Cody Morterud <cody22597@gmail.com>
2017-02-23 13:02:49 -0500

    Made possible fix to accumSummary_scale.cpp

--- SEMCPP/src/accumSummary_scale.cpp
+++ SEMCPP/src/accumSummary_scale.cpp
@@ -26,6 +26,10 @@ void accumSummary_scale(Dataset &data, const string &hfile, const string &cfile,
 	strcpy(fname, hfile.c_str());
 	const char* const mode = "r";
 	bigWigFile_t *bwFile = bwOpen(fname, NULL, mode);
+    if(bwFile == NULL){
+        cerr << "Failed to open hfile: " << hfile << endl;
+        exit(1);
+    }
 
 	int dist = 500;
 	int total_size = dist * 2 + scale;
@@ -49,7 +53,7 @@ void accumSummary_scale(Dataset &data, const string &hfile, const string &cfile,
 	char *chrom = nullptr;
 	string line = "", seqid = "", direction = "";
 	vector<string> temp, signal_array(total_size);
-	int start = 0, end = 0, counter = 0; 
+	int start = 0, end = 0, counter = 0;
     int upstart = 0, upend = 0;
 	// pointer to hold double values from library function;
 	double *values = nullptr;
@@ -76,7 +80,7 @@ void accumSummary_scale(Dataset &data, const string &hfile, const string &cfile,
 #endif
                 }
 		}
-        
+
 		// if line doesn't begin with chr
 		else{
 			seqid = "chr" + temp[0];
@@ -95,19 +99,19 @@ void accumSummary_scale(Dataset &data, const string &hfile, const string &cfile,
 
 		// stats function used from library
 		//double *bwStats(bigWigFile_t *fp, char *chrom, uint32_t start, uint32_t end, uint32_t nBins, enum bwStatsType type);
-		values = bwStats(bwFile, chrom, static_cast<uint32_t>(0), static_cast<uint32_t>(total_size), static_cast<uint32_t>(total_size), type);
-    if(values == NULL){
-      cerr << "Failure to use bwStats!\n\tEXITING\n";
-      exit(1);
-    }
-		counter = 0;
+		values = bwStats(bwFile, chrom, static_cast<uint32_t>(upstart), static_cast<uint32_t>(upend), static_cast<uint32_t>(upend - upstart), type);
+        if(values == NULL){
+          cerr << "Failure to use bwStats!\n\tEXITING\n";
+          exit(1);
+        }
+    		counter = 0;
 
-		for(counter = 0; counter < total_size; counter++){
-			values[counter] = roundf(values[counter] * 1000) / 1000;
-			signal_array[counter] = values[counter];
-		}
+    		for(counter = 0; counter < total_size; counter++){
+    			values[counter] = roundf(values[counter] * 1000) / 1000;
+    			signal_array[counter] = values[counter];
+    		}
 
-    free(values);
+        free(values);
 		delete [] chrom;
 
 		//output results
@@ -115,15 +119,17 @@ void accumSummary_scale(Dataset &data, const string &hfile, const string &cfile,
 			for(int k = 0; k < total_size; k++)
 				// need to determine how to check for definition of signal_array[k]
 				output[k] = signal_array[k];
-		else
+		else{
+            reverse(signal_array.begin(), signal_array.end());
 			for(int k = total_size - 1; k >= 0; k--)
 				output[k] = signal_array[k];
+            }
 
 
 		max = 0;
 		hitcount = 0;
 		for(int l = 0; l < static_cast<int>(output.size()); l++){
-			if(stod(output[l]) > max)
+			if(stod(output[l]) > max) // string to double
 				max = stod(output[l]);
 			if(output[l] != "N")
 				hitcount++;
