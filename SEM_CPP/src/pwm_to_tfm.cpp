9467fe0bb214ccbe038ad2da0bd8f1486b5b198f
Alan Boyle <apboyle@umich.edu>
2018-01-16 14:13:37 -0500

    These columns were backwards

--- SEM_CPP/src/pwm_to_tfm.cpp
+++ SEM_CPP/src/pwm_to_tfm.cpp
@@ -7,14 +7,15 @@ using namespace std;
 //MODIFIES: TFM_data within data
 //EFFECTS: puts a, c, g, t data into tfm_format
 void pwm_to_tfm(Dataset & data){
+
 	data.TFM_data.letter_array[0].clear();
 	data.TFM_data.letter_array[1].clear();
 	data.TFM_data.letter_array[2].clear();
 	data.TFM_data.letter_array[3].clear();
-	for(int i = 0; i < (int)data.PWM_data.matrix_arr.size(); ++i){
-		data.TFM_data.letter_array[0].push_back(data.PWM_data.matrix_arr[i][0]);
-		data.TFM_data.letter_array[1].push_back(data.PWM_data.matrix_arr[i][1]);
-		data.TFM_data.letter_array[2].push_back(data.PWM_data.matrix_arr[i][2]);
-		data.TFM_data.letter_array[3].push_back(data.PWM_data.matrix_arr[i][3]);
+	for(int i = 0; i < (int)data.PWM_data.matrix_arr[0].size(); ++i){
+		data.TFM_data.letter_array[0].push_back(data.PWM_data.matrix_arr[0][i]);
+		data.TFM_data.letter_array[1].push_back(data.PWM_data.matrix_arr[1][i]);
+		data.TFM_data.letter_array[2].push_back(data.PWM_data.matrix_arr[2][i]);
+		data.TFM_data.letter_array[3].push_back(data.PWM_data.matrix_arr[3][i]);
 	}
 }
