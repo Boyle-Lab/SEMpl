40669f0644b2be43d8385cba1d427a4cc33f7c51
Alan Boyle <apboyle@umich.edu>
2018-01-16 14:16:25 -0500

    pipe bowtie right to bedtools to save on IO - messy

--- SEM_CPP/src/bowtie_genome_map.cpp
+++ SEM_CPP/src/bowtie_genome_map.cpp
@@ -6,20 +6,6 @@
 using namespace std;
 
 
-//From https://stackoverflow.com/questions/478898/how-to-execute-a-command-and-get-output-of-command-within-c-using-posix
-std::stringstream exec(const char* cmd) {
-    std::array<char, 128> buffer;
-    std::stringstream result;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    while (!feof(pipe.get())) {
-        if (fgets(buffer.data(), 128, pipe.get()) != nullptr)
-            result << buffer.data();
-    }
-    return result;
-}
-
-
 // if I understand correctly, bowtie places output in the filename specified
 // at the end of the command                              // intermediate.dat, as
 //                                                        // defined in iterativeSEM.hpp
@@ -30,11 +16,14 @@ std::stringstream exec(const char* cmd) {
 
 
 void bowtie_genome_map(int length, const string& genome, const string& file, 
-                       const string& final_output, bool verbose){
+                       const string& final_output, const string& dnase_file, bool verbose){
     //const char *argvs[9] = {"./bin/bowtie", "--quiet", "-a", "-v 0", 
     //                        genome.c_str(), "-f", file.c_str(), "temp.dat" };
 
-    string cmd = "./bin/bowtie --threads 5 --quiet -a -v 0 ./data/hg19 -f " + file;
+//    string cmd = "./bin/bowtie --threads 10 --quiet -a -v 0 ./data/hg19 -f " + file;
+    string cmd = "./bin/bowtie --threads 5 --quiet -a -v 0 ./data/hg19 -f " + file 
+       + " | awk '{print $3\"\\t\"$4\"\\t\"$4+" + std::to_string(length) + "\"\\t\"$5\"\\t\"$2}' | " 
+       + "./bin/bedtools intersect -a stdin -b " + dnase_file + " -wa -u | sort | uniq";
 
     if(verbose){
         cout << "Running command: " << cmd << "\n\tRunning...." << flush;
@@ -57,6 +46,8 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
            //cout << map_line << endl << flush;
         #endif
         split_string(map_line, "\t", dat);
+
+/*
         if(dat[1] == "+"){
             DNA = dat[4];
         }
@@ -72,6 +63,21 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
         OUT << dat[2] << '\t' << dat[3] << '\t'
             << atoi(dat[3].c_str()) + length
             << '\t' << DNA << '\t' << dat[1] << '\n';
+*/
+        if(dat[4] == "+"){
+            DNA = dat[3];
+        }
+        else if(dat[4] == "-"){
+            DNA = revCompDNA(dat[3]);
+        }
+        else{
+            cerr << "unknown strand!\n";
+            cerr << map_line << endl;
+            exit(1);
+        }
+
+        OUT << dat[0] << '\t' << dat[1] << '\t'
+            << dat[2] << '\t' << DNA << '\t' << dat[4] << '\n';
 
         dat.clear();
     }
@@ -80,6 +86,7 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
         cout << "FINISH" << endl;
     }
 
+    OUT.close();
 
 }
 
