f210dbde764653d651fb3ef91ec674969bf5993e
Alan Boyle <apboyle@umich.edu>
2018-01-23 14:54:05 -0500

    Cleaned up and commented

--- SEM_CPP/src/bowtie_genome_map.cpp
+++ SEM_CPP/src/bowtie_genome_map.cpp
@@ -6,29 +6,18 @@
 using namespace std;
 
 
-// if I understand correctly, bowtie places output in the filename specified
-// at the end of the command                              // intermediate.dat, as
-//                                                        // defined in iterativeSEM.hpp
-// genome is "./data/hg19", DNA_FA_FILE  is the file that contains .fa
-
-
-//int bowtie(int argc, const char **argv);
-
-
+// Takes fasta file and aligns to genome, filters by DNase, and sorts
+//  stores results in final output
 void bowtie_genome_map(int length, const string& genome, const string& file, 
                        const string& final_output, const string& dnase_file, bool verbose){
-    //const char *argvs[9] = {"./bin/bowtie", "--quiet", "-a", "-v 0", 
-    //                        genome.c_str(), "-f", file.c_str(), "temp.dat" };
 
-//    string cmd = "./bin/bowtie --threads 10 --quiet -a -v 0 ./data/hg19 -f " + file;
-    string cmd = "./bin/bowtie --threads 10 --quiet -a -v 0 ./data/hg19 -f " + file 
+    string cmd = "./bin/bowtie --threads 40 --quiet -a -v 0 ./data/hg19 -f " + file 
        + " | awk '{print $3\"\\t\"$4\"\\t\"$4+" + std::to_string(length) + "\"\\t\"$5\"\\t\"$2}' | " 
        + "./bin/bedtools intersect -a stdin -b " + dnase_file + " -wa -u | sort | uniq";
 
     if(verbose){
         cout << "Running command: " << cmd << "\n\tRunning...." << flush;
     }
-
 
     ofstream OUT(final_output);
     if(!OUT){
@@ -37,33 +26,16 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
     }
 
 
-    std::stringstream IN = exec(cmd.c_str());
+    std::stringstream IN = exec(cmd.c_str()); // stringstream instead of writing to a file
     string map_line = "";
     vector<string> dat;
     string DNA = "";
+
+    // Read output, reformat, and write to final_output file
+    // This could all be kept in memory in the future.
     while(std::getline(IN, map_line, '\n')){
-        #ifdef DEBUG
-           //cout << map_line << endl << flush;
-        #endif
         split_string(map_line, "\t", dat);
 
-/*
-        if(dat[1] == "+"){
-            DNA = dat[4];
-        }
-        else if(dat[1] == "-"){
-            DNA = revCompDNA(dat[4]);
-        }
-        else{
-            cerr << "unknown strand!\n";
-            cerr << map_line << endl;
-            exit(1);
-        }
-
-        OUT << dat[2] << '\t' << dat[3] << '\t'
-            << atoi(dat[3].c_str()) + length
-            << '\t' << DNA << '\t' << dat[1] << '\n';
-*/
         if(dat[4] == "+"){
             DNA = dat[3];
         }
@@ -87,6 +59,5 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
     }
 
     OUT.close();
-
 }
 
