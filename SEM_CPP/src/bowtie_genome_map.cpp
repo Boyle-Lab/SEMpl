99f682cc9e5d7977f4dcece381aae5547d7a90a4
Alan Boyle <apboyle@umich.edu>
2018-05-09 13:20:17 -0400

    fork to run external program

--- SEM_CPP/src/bowtie_genome_map.cpp
+++ SEM_CPP/src/bowtie_genome_map.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <sstream>
+#include <unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
 
@@ -12,55 +14,32 @@ using namespace std;
 void bowtie_genome_map(int length, const string& genome, const string& file, 
                        const string& final_output, const string& dnase_file, int threads, bool verbose){
 
-    string cmd = "./bin/bowtie --threads " + std::to_string(threads) + " --quiet -a -v 0 ./data/hg19 -r " + file 
-       + " | awk '{print $3\"\\t\"$4\"\\t\"$4+" + std::to_string(length) + "\"\\t\"$5\"\\t\"$2}' | " 
-       + "./bin/bedtools intersect -a stdin -b " + dnase_file + " -wa -u | sort | uniq";
+    string cmd = "./map-filer " + std::to_string(length) + " " + genome + " " + file + " " + final_output + " " + dnase_file + " " + std::to_string(threads) + " " + std::to_string(verbose);
 
     if(verbose){
         cout << "Running command: " << cmd << "\n\tRunning...." << flush;
     }
 
-    ofstream OUT(final_output);
-    if(!OUT){
-        cerr << "Failure to open \"" << final_output << "\".\n";
-        exit(1);
-    }
-
-
-    std::stringstream IN = exec(cmd.c_str()); // stringstream instead of writing to a file
-    string map_line = "";
-    vector<string> dat;
-    string DNA = "";
-
-    // Read output, reformat, and write to final_output file
-    // This could all be kept in memory in the future.
-    while(std::getline(IN, map_line, '\n')){
-        split_string(map_line, "\t", dat);
-
-        if(dat[4] == "+"){
-            DNA = dat[3];
+    bool bad_run = false;
+    pid_t pid;
+    int child_status;
+    do {
+        pid = fork();
+        if(pid == 0) { /* Child */
+            exec(cmd.c_str());
+            exit(0);
+        } else { /* Parent */
+            wait(&child_status);
+            if(child_status != 0) {
+                cerr << "Status: " << child_status << " Need to run the process again!\n" << flush;
+                bad_run = true;
+            }
         }
-        else if(dat[4] == "-"){
-            DNA = revCompDNA(dat[3]);
-        }
-        else{
-            cerr << "unknown strand!\n";
-            cerr << map_line << endl;
-            exit(1);
-        }
-
-        OUT << dat[0] << '\t' << dat[1] << '\t'
-            << dat[2] << '\t' << DNA << '\t' << dat[4] << '\n';
-
-        dat.clear();
-    }
+    } while(bad_run);
 
     if(verbose){
         cout << "FINISH" << endl;
     }
 
-    OUT.close();
-
-    IN.str(std::string());
 }
 
