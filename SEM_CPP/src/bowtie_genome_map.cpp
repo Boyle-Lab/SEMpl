466c6b1e9ba4023b03ee7727bd3519319fcaff31
Alan Boyle <apboyle@umich.edu>
2018-07-02 15:02:28 -0400

    revert to keeping bowtie internal

--- SEM_CPP/src/bowtie_genome_map.cpp
+++ SEM_CPP/src/bowtie_genome_map.cpp
@@ -12,37 +12,36 @@ using namespace std;
 void bowtie_genome_map(int length, const string& genome, const string& file, 
                        const string& final_output, const string& dnase_file, int threads, bool verbose){
 
-    const string temp_file = "temp_bowtie.dat";
-    string cmd = "./bin/bowtie --threads " + std::to_string(threads) + " --quiet -a -v 0 ./data/hg19 -r " + file + " > /dev/null";
+    string cmd = "./bin/bowtie --threads " + std::to_string(threads) + " --quiet -a -v 0 ./data/hg19 -r " + file 
+       + " | awk '{print $3\"\\t\"$4\"\\t\"$4+" + std::to_string(length) + "\"\\t\"$5\"\\t\"$2}' | " 
+       + "./bin/bedtools intersect -a stdin -b " + dnase_file + " -wa -u | sort | uniq";
 
     if(verbose){
         cout << "Running command: " << cmd << "\n\tRunning...." << flush;
     }
 
-//    ofstream OUT(temp_file);
-//    if(!OUT){
-//        cerr << "Failure to open \"" << temp_file << "\".\n";
-//        exit(1);
-//    }
+    ofstream OUT(final_output);
+    if(!OUT){
+        cerr << "Failure to open \"" << final_output << "\".\n";
+        exit(1);
+    }
 
-    exec(cmd.c_str());
-//    std::stringstream IN = exec(cmd.c_str()); // stringstream instead of writing to a file
-//    string map_line = "";
-//    vector<string> dat;
-//    string DNA = "";
-//    int DNAlen;
 
-/*
-    // Read output, reformat, and write to temp file
+    std::stringstream IN = exec(cmd.c_str()); // stringstream instead of writing to a file
+    string map_line = "";
+    vector<string> dat;
+    string DNA = "";
+
+    // Read output, reformat, and write to final_output file
     // This could all be kept in memory in the future.
     while(std::getline(IN, map_line, '\n')){
         split_string(map_line, "\t", dat);
 
-        if(dat[1] == "+"){
-            DNA = dat[4];
+        if(dat[4] == "+"){
+            DNA = dat[3];
         }
-        else if(dat[1] == "-"){
-            DNA = revCompDNA(dat[4]);
+        else if(dat[4] == "-"){
+            DNA = revCompDNA(dat[3]);
         }
         else{
             cerr << "unknown strand!\n";
@@ -50,37 +49,18 @@ void bowtie_genome_map(int length, const string& genome, const string& file,
             exit(1);
         }
 
-//        if(std::stoi(dat[6]) > 0) {  // bowtie sometimes returns alignments with first base missing
-        DNAlen = DNA.length();
-        if(DNAlen == length) {
-          OUT << dat[2] << '\t' << dat[3] << '\t'
-              << std::stoi(dat[3]) + length << '\t' << DNA << '\t' << dat[1] << '\n';
-        }
+        OUT << dat[0] << '\t' << dat[1] << '\t'
+            << dat[2] << '\t' << DNA << '\t' << dat[4] << '\n';
 
         dat.clear();
     }
-*/
-    if(verbose){
-        cout << "Bowtie complete..." << endl;
-    }
-
-//    OUT.close();
-//    IN.str(std::string());
-
-    cmd = "./bin/bedtools intersect -a " + temp_file + " -b " + dnase_file + " -wa -u | sort | uniq > " + final_output;
-
-    if(verbose){
-        cout << "Running command: " << cmd << "\n\tRunning...." << flush;
-    }
-
-    exec(cmd.c_str()); // stringstream instead of writing to a file
 
     if(verbose){
         cout << "FINISH" << endl;
     }
 
+    OUT.close();
+
+    IN.str(std::string());
 }
 
-int main(int argc, char* argv[])
-{
-    
